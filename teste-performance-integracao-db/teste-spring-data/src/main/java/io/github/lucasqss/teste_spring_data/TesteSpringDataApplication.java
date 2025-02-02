6c3a1bb37666109583328193e1beb91055d413dd
lucasqss <lqsilvas@gmail.com>
2025-02-02 14:44:16 -0300

    temp

---
+++ teste-performance-integracao-db/teste-spring-data/src/main/java/io/github/lucasqss/teste_spring_data/TesteSpringDataApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucasqss.teste_spring_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSpringDataApplication.class, args);
+	}
+
+}
---
+++ teste-performance-integracao-db/teste-spring-data/src/test/java/io/github/lucasqss/teste_spring_data/TesteSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucasqss.teste_spring_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
